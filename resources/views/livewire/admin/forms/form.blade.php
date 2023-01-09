1cfaf47b1d6b362d402bb0f0207380d597dc3d3b
Rakesh Kapoor <rkapoor@ishir.com>
2023-01-09 14:38:23 +0530

    test compoents creation

--- resources/views/livewire/admin/forms/form.blade.php
+++ resources/views/livewire/admin/forms/form.blade.php
@@ -1,4 +1,33 @@
 <div>
+    <x-tab name="company">
+
+    <x-tabs.header>
+        <x-tabs.link name="details">Details</x-tabs.link>
+        <x-tabs.link name="company">Company</x-tabs.link>
+        <x-tabs.link name="team">Team</x-tabs.link>
+    </x-tabs.header>
+
+    <x-tabs.div name="details">
+        <p>Details</p>
+    </x-tabs.div>
+
+    <x-tabs.div name="company">
+        <p>Company</p>
+    </x-tabs.div>
+
+    <x-tabs.div name="team">
+        <p>Team</p>
+    </x-tabs.div>
+
+</x-tab>
+
+    <x-accordian  >
+         <x-slot name="lefta">
+            <h3>Rolesdd</h3>
+            <p>dadfasdf .</p>
+        </x-slot>
+    </x-accordian>
+
     <div class="card">
 
         <div class="flex justify-between">
